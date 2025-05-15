618607d540334a507ab1083c996f8a685439d945
周心怡 <monicachou.688@gmail.com>
2025-05-15 13:57:16 +0000

    0515

--- lesson6/lesson6_4.py
+++ lesson6/lesson6_4.py
@@ -1,11 +1,11 @@
-import tools #呼叫module
+from tools import calculate_bmi,get_state #呼叫module
 
 def main():
     # BMI計算
     try:
         height:int = int(input('請輸入你的身高(公分 cm):'))        
         weight = eval(input('請輸入你的體重(公斤 kg):'))        
-        bmi = tools.calculate_bmi(height,weight)#要加上tools.參數
+        bmi = calculate_bmi(height,weight)
     except ValueError:
         print('輸入發生錯誤')
     except Exception as e :
@@ -14,7 +14,7 @@ def main():
         print(f'你的身高:{height} 公分')
         print(f'你的體重:{weight:.2f} 公斤')
         print(f'你的 BMI值為:{bmi:.2f}')
-        print(tools.get_state(bmi))#要加上tools.參數
+        print(get_state(bmi))
 
 if __name__ == "__main__":
     main()
--- lesson6/lesson6_5.py
+++ lesson6/lesson6_5.py
@@ -1,11 +1,13 @@
-import tools #呼叫module
+#import tools
+#from tools import calculate_bmi, get_state
+import edu
 
 def main():
     # BMI計算
     try:
         height:int = int(input('請輸入你的身高(公分 cm):'))        
         weight = eval(input('請輸入你的體重(公斤 kg):'))        
-        bmi = tools.calculate_bmi(height,weight)#要加上tools.參數
+        bmi = edu.tools.calculate_bmi(height,weight)
     except ValueError:
         print('輸入發生錯誤')
     except Exception as e :
@@ -14,7 +16,7 @@ def main():
         print(f'你的身高:{height} 公分')
         print(f'你的體重:{weight:.2f} 公斤')
         print(f'你的 BMI值為:{bmi:.2f}')
-        print(tools.get_state(bmi))#要加上tools.參數
+        print(edu.tools.get_state(bmi))#呼叫edu資料夾pakeage
 
 if __name__ == "__main__":
     main()
